87ed3958dc85f025f503e6610b2586dae23797bf
Liao Wen Chung <wcliao1962@gmail.com>
2020-11-07 10:38:38 +0800

    主樣板 & 共同區塊

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>@yield('title')</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="{{asset('css/all.min.css')}}" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="{{asset('css/clean-blog.min.css')}}" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+ @include('layouts.partials.navigations')
+
+  <!-- Page Header -->
+
+
+  <!-- Main Content -->
+  @yield('content')
+
+
+  <hr>
+
+  <!-- Footer -->
+  @include('layouts.partials.footer')
+
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="{{asset('js/jquery.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="{{asset('js/clean-blog.min.js')}}"></script>
+
+</body>
+
+</html>
