62d62c6a0e148f241053945da9dbb83edb1706fa
Liao Wen Chung <wcliao1962@gmail.com>
2020-11-08 14:51:00 +0800

    0825_u1_練習4-3: 更新資料(save方法, 非mass assignment)

--- routes/web.php
+++ routes/web.php
@@ -35,12 +35,16 @@ Route::get('/', function () {
 //    $posts=Post::where('id', '<', "10")->orderBy('id','DESC')->get();
 //    dd($posts);
 
-    $post= Post::find(1);
-    $post->update([
-        'title'=>'updated title',
-        'content'=>'updated content'
-    ]);
+//    $post= Post::find(1);
+//    $post->update([
+//        'title'=>'updated title',
+//        'content'=>'updated content'
+//    ]);
 
+    $post= Post::find(1);
+    $post->title = 'saved title';
+    $post->content = 'saved content';
+    $post->save();
 
 });
 
