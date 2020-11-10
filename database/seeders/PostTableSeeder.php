3219e6e0ff2c7a4e8d114954d84c9dc8a29f14fa
Liao Wen Chung <wcliao1962@gmail.com>
2020-11-10 23:34:27 +0800

    0825_u2_練習2

--- database/seeders/PostTableSeeder.php
+++ database/seeders/PostTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class PostTableSeeder extends Seeder
@@ -13,6 +14,11 @@ class PostTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (range(1,20) as $number) {
+            Post::create([
+                'title'=>'title'.$number,
+                'content'=>'content'.$number,
+            ]);
+        }
     }
 }
