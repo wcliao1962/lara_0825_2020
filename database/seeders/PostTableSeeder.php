be7f2d091031ed1d15a09ed95e426adb512a522a
Liao Wen Chung <wcliao1962@gmail.com>
2020-11-10 23:56:59 +0800

    0825_u2_練習4-1, 4-2

--- database/seeders/PostTableSeeder.php
+++ database/seeders/PostTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Post;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class PostTableSeeder extends Seeder
@@ -14,10 +15,16 @@ class PostTableSeeder extends Seeder
      */
     public function run()
     {
+        Post::truncate();
+        $total=20;
+
         foreach (range(1,20) as $number) {
             Post::create([
                 'title'=>'title'.$number,
                 'content'=>'content'.$number,
+                'is_feature'=>rand(0,1),
+                'created_at'=>Carbon::now()->subDays($total-$number),
+                'updated_at'=>Carbon::now()->subDays($total-$number)->addHours(rand(1,24)),
             ]);
         }
     }
