d91b2a91a4946375db5847b02fbb829b53d61a52
Liao Wen Chung <wcliao1962@gmail.com>
2020-11-11 00:01:42 +0800

    0825_u2_練習4-3

--- database/seeders/PostTableSeeder.php
+++ database/seeders/PostTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Post;
 use Carbon\Carbon;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class PostTableSeeder extends Seeder
@@ -17,11 +18,12 @@ class PostTableSeeder extends Seeder
     {
         Post::truncate();
         $total=20;
+        $faker=Factory::create('zh_TW');
 
         foreach (range(1,20) as $number) {
             Post::create([
-                'title'=>'title'.$number,
-                'content'=>'content'.$number,
+                'title'=> $faker->sentence,
+                'content'=>$faker->paragraph,
                 'is_feature'=>rand(0,1),
                 'created_at'=>Carbon::now()->subDays($total-$number),
                 'updated_at'=>Carbon::now()->subDays($total-$number)->addHours(rand(1,24)),
